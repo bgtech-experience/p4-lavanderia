b97bd92ea6dea8ad6968d65e0bcff2a9a1ba3c51
BGTech - Experience <bgtech.experience@gmail.com>
2024-11-03 15:00:35 -0300

    Estrutura básica (scafold) do Site em React, com Routes e Components.

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,11 @@
+import './logo.css';
+
+export default function Logo({ children }) {
+    return (
+        <div className="logo">
+            {/* { children } */}
+
+            <div>BGTech</div> <span>Experience</span>
+        </div>
+    );
+}
